b1ef86f6b3a64e3111b124bca8256ade40ca20e9
sushant <sushantyelpale@gmail.com>
2017-09-07 18:10:57 +0530

    All the changes committed.

---
+++ DALOptimizer/MatchExpr.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using ICSharpCode.NRefactory.CSharp;
+using ICSharpCode.NRefactory.CSharp.Refactoring;
+using ICSharpCode.NRefactory.CSharp.Resolver;
+using ICSharpCode.NRefactory.Editor;
+using ICSharpCode.NRefactory.PatternMatching;
+using ICSharpCode.NRefactory.TypeSystem;
+
+namespace DALOptimizer
+{
+    class MatchExpr
+    {
+        public void initializeExpr(AstNode expr, CSharpAstResolver astResolver, ICompilation compilation)
+        {
+            
+        }
+        
+    }
+}
---
+++ DALOptimizer/PrintFunction.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+using ICSharpCode.NRefactory.CSharp;
+
+namespace DALOptimizer
+{
+    class PrintFunction
+    {
+        public void PrintMethod(Solution solution)
+        {
+
+            Console.WriteLine("Found {0} places to refactor Field Declaration in {1} files.",
+                  solution.AllFiles.Sum(f => f.IndexOfFieldDecl.Count),
+                  solution.AllFiles.Count(f => f.IndexOfFieldDecl.Count > 0));
+            Console.WriteLine("Found {0} places to refactor Property Declaration in {1} files.",
+                  solution.AllFiles.Sum(f => f.IndexOfPropDecl.Count),
+                  solution.AllFiles.Count(f => f.IndexOfPropDecl.Count > 0));
+            Console.WriteLine("Found {0} places to refactor Finally Block in {1} files.",
+                  solution.AllFiles.Sum(f => f.IndexOfBlockStmt.Count),
+                  solution.AllFiles.Count(f => f.IndexOfBlockStmt.Count > 0));
+            Console.WriteLine("Found {0} places to refactor assignment Expressions in {1} files.",
+                  solution.AllFiles.Sum(f => f.IndexOfAssExpr.Count),
+                  solution.AllFiles.Count(f => f.IndexOfAssExpr.Count > 0));
+            Console.WriteLine("Found {0} places to refactor Expression statement in {1} files.",
+                  solution.AllFiles.Sum(f => f.IndexOfExprStmt.Count),
+                  solution.AllFiles.Count(f => f.IndexOfExprStmt.Count > 0));
+            Console.WriteLine("Found {0} places to refactor Catch clause in {1} files.",
+                  solution.AllFiles.Sum(f => f.IndexOfCtchClause.Count),
+                  solution.AllFiles.Count(f => f.IndexOfCtchClause.Count > 0));
+            Console.WriteLine("Found {0} places to refactor Method Declaration in {1} files.",
+                  solution.AllFiles.Sum(f => f.IndexOfMthdDecl.Count),
+                  solution.AllFiles.Count(f => f.IndexOfMthdDecl.Count > 0));
+            Console.WriteLine("Found {0} places to refactor Variable Declaration in {1} files.",
+                  solution.AllFiles.Sum(f => f.IndexOfVarDeclStmt.Count),
+                  solution.AllFiles.Count(f => f.IndexOfVarDeclStmt.Count > 0));
+        }
+
+        public void PrintInvocation(AstNode invocation)
+        {
+            Console.WriteLine("new_Line:" + invocation.GetRegion().Begin.Line + "  Invocation: " + invocation.GetText().ToString());
+        }
+    }
+}
